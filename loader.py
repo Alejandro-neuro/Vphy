49e815e2db7523a2ab06b3bc5d13a5cb0e59650c
Alejandro <m.a.castanedagarcia@tudelft.nl>
2023-08-07 15:30:17 +0200

    latest updates

--- loader.py
+++ loader.py
@@ -3,132 +3,59 @@ import torch.nn as nn
 import networkx as nx
 import numpy as np
 
+from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mutual_info_score
 
 from omegaconf import OmegaConf
 
+import Data.genData as genData
+from torchvision import transforms
 
 
-def gen_Dataset(x, type, window=20, overlap=0.5):
-    dataset = []
-    
+class Dataset(torch.utils.data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, x, dt,  transform=None):
+        'Initialization'
+        self.x = x
+        self.dt = dt
+        self.transform = None
+        self.convert_tensor = transforms.ToTensor()
 
-    for i in range( x.shape[1] - window-1 ):
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.x)-2
 
-        datax = x[:,i:i+window]
-        label = x[:,i+window+1]
-
-        adj = generateAdjacencyMatrix(datax,type)
-        G = nx.DiGraph(adj)
-        data1 = from_networkx(G)  
-
-        data = data1.clone()
-        data['x']=torch.from_numpy(datax).float()
-        data['y']=torch.from_numpy(label).float()        
-        data['weight']=data['weight'].float()
-
-        dataset.append(data)
-
-        i = i + int(window * overlap)
-
-    return dataset
-
-def timeSeries2Dataset(timeseries, type, window=20, overlap=0.5):
-
-    data = timeseries
-
-    len_data = data.shape[1]
-
-    dataset={
-        "trn": {
-        "data": None,
-        "labels": None
-        },
-        "val":  {
-        "data": None,
-        "labels": None
-        },
-        "tst":  {
-        "data": None,
-        "labels": None
-        }
-    }
-
-    cfg = OmegaConf.load("config.yaml")
-
-    train_size = cfg.dataset.train_size
-    val_size = cfg.dataset.val_size
-    test_size = cfg.dataset.test_size
-
-    X_train = data[:,:int(train_size*len_data)]
-    X_val = data[:,int(train_size*len_data):int((train_size+val_size)*len_data)]
-    X_test = data[:,int((train_size+val_size)*len_data):]   
-
-    print("X_train shape: ", X_train.shape)
-    print("X_val shape: ", X_val.shape)
-    print("X_test shape: ", X_test.shape)
-
-    dataset['trn'] = gen_Dataset(X_train, type, window=window, overlap=0.5)
-    dataset['val'] = gen_Dataset(X_val, type, window=window, overlap=0.5)
-    dataset['tst'] = gen_Dataset(X_test, type, window=window, overlap=0.5)
-    
-    return dataset
-
-
-
-
-def create_partitions(dataset):
-    
-    train_loader = DataLoader(dataset['trn'], batch_size=64, shuffle=True)
-    val_loader = DataLoader( dataset['val'], batch_size=64, shuffle=False)
-    test_loader = DataLoader( dataset['tst'], batch_size=64, shuffle=False)
- 
-    return train_loader,val_loader,test_loader
-
-#create a function that returns adjacency matrix and based on the input string "type" and returns pearson correlation
-#or mutual information
-def generateAdjacencyMatrix(x,type):
-    
-    if(type == 'pearson'): 
-    #calculate adjacency matrix with pearson correlation
-        adj = np.zeros((x.shape[0],x.shape[0]))
-        for i in range(x.shape[0]):
-            for j in range(x.shape[0]):
-                adj[i,j] = np.corrcoef(x[i,:],x[j,:])[0,1] #calculate pearson correlation between nodes i and j
-                adj[j,i] = adj[i,j] #make the matrix symmetric
-
-        return adj
-
-    
-    if(type == 'mutual_info'):
-        adj = np.zeros((x.shape[0],x.shape[0]))
-        for i in range(x.shape[0]):
-            for j in range(x.shape[0]):
-                adj[i,j] = mutual_info_score(x[i,:],x[j,:]) #calculate mutual information between nodes i and j
-                adj[j,i] = adj[i,j] #make the matrix symmetric  
-        return adj
-    
-    #calculate adjacency matrix with cosine similarity
-    if(type == 'cosine'):
-        adj = np.zeros((x.shape[0],x.shape[0]))
-        for i in range(x.shape[0]):
-            for j in range(x.shape[0]):
-                adj[i,j] = np.dot(x[i,:],x[j,:]) / (np.linalg.norm(x[i,:]) * np.linalg.norm(x[j,:]))
-                adj[j,i] = adj[i,j] #make the matrix symmetric
-        return adj   
-
-    return adj
+  def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        x0 =self.convert_tensor( genData.create_pendulum_image( self.x[index] ) )
         
+        x1 =self.convert_tensor( genData.create_pendulum_image( self.x[index+1] ) )
+  
+        x2 =self.convert_tensor( genData.create_pendulum_image( self.x[index+2] ) )
 
-def generateLoaders( timeseries, type='pearson', window=20, overlap=0.5):
 
-    dataset = timeSeries2Dataset(timeseries, type, window, overlap)
+        if self.transform:
+            x0 = self.transform(x0)
+            x1 = self.transform(x1)
+            x2 = self.transform(x2)
+ 
 
-    train_loader,val_loader,test_loader = create_partitions(dataset)
+        return (x0,x1 ), x2
+        
+def getLoader(X):
+     
+      #split dataset 80-20 for training and validation
 
-    return train_loader,val_loader,test_loader
+      train_x, val_x = train_test_split(X, test_size=0.2, shuffle=False)
+
+      #create train and test dataloaders
+
+      train_dataset = DataLoader( Dataset(train_x, 1/30), batch_size=32, shuffle=False)
+      val_dataset = DataLoader( Dataset(val_x, 1/30), batch_size=32, shuffle=False)    
+
+      return train_dataset, val_dataset, train_x, val_x 
 
 if __name__ == "__main__":
-    generateLoaders()
-
+    pass
--- models.py
+++ models.py
@@ -1,3 +1,100 @@
 import torch
 import torch.nn as nn
 
+class Encoder(nn.Module):
+    def __init__(self, initw = False):
+        super().__init__()
+
+        #Conv layers
+        self.Conv = nn.Conv2d(16, 33, 3, stride=2)
+
+        #Linear layers
+        self.l1 = nn.Linear(101,50 )
+        self.l2 = nn.Linear(50,10 )
+        self.l3 = nn.Linear(10,1 )
+        
+        #Activation functions
+        self.sigmoid = nn.Sigmoid()
+        self.Tanh=nn.Tanh()
+        self.relu= nn.ReLU()
+
+        if initw:
+          nn.init.xavier_normal(self.l1.weight)
+          nn.init.xavier_normal(self.l2.weight)
+          nn.init.xavier_normal(self.l3.weight)
+
+    def forward(self, x):
+      x=torch.flatten(x,1)
+      x = self.Tanh(self.l1(x))
+      x = self.Tanh(self.l2(x))
+      x = self.l3(x)
+
+      return x
+
+class pModel(nn.Module):
+    def __init__(self, initw = False):
+        super().__init__()
+        self.alpha = torch.tensor([-2.00], requires_grad=True).float()
+        self.beta = torch.tensor([-2500.00], requires_grad=True).float()
+        #self.alpha = nn.Parameter(self.alpha )
+        #self.beta= nn.Parameter(self.beta)
+
+    def forward(self, x0,x1,dt):
+      
+
+
+      return x1+(x1-x0)+self.alpha*(x1-x0 )*dt*2 + (self.beta*x1 )*dt*dt*4
+
+class Decoder(nn.Module):
+    def __init__(self, initw = False):
+        super().__init__()
+        self.l1 = nn.Linear(1,10 )
+        self.l2 = nn.Linear(10,50 )
+        self.l3 = nn.Linear(50,101 )
+
+        self.uflat = nn.Unflatten(1, torch.Size([101,1]))
+        self.relu= nn.ReLU()
+        self.Softmax= nn.Softmax(dim=1)
+        self.sigmoid= nn.Sigmoid()
+
+        self.Tanh=nn.Tanh()
+
+        if initw:
+
+          #uniform_
+          #xavier_normal
+          nn.init.xavier_normal(self.l1.weight)
+          nn.init.xavier_normal(self.l2.weight)
+          nn.init.xavier_normal(self.l3.weight)
+
+    def forward(self, x):
+
+      x = self.Tanh(self.l1(x))
+      x = self.Tanh(self.l2(x))
+      x = self.sigmoid(self.l3(x))
+
+      x = self.uflat(x)
+      return x
+
+
+
+class AE(nn.Module):
+    def __init__(self, dt, initw = False):
+        super().__init__()
+
+        self.encoder     = Encoder(initw=initw )
+        self.decoder     = Decoder(initw=initw)
+        self.pModel     =  pModel(initw=initw)
+        self.dt = dt
+
+    def forward(self, x0,x1):
+
+      z0 =self.encoder(x0)
+      z1 =self.encoder(x1)
+      z2 =self.pModel(z0,z1, self.dt)
+
+      in0Rec =self.decoder(z0)
+      in1Rec =self.decoder(z1)
+      outRec =self.decoder(z2)
+
+      return (z0,z1,z2), (in0Rec,in1Rec,outRec)
