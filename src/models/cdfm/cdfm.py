bce2367489ed5b5a7bde728bd1417a66974273a1
Alejandro-neuro <m.a.castanedagarcia@tudelft.nl>
2024-03-20 15:49:09 +0100

    retuirn model from difussion

--- src/models/cdfm/cdfm.py
+++ src/models/cdfm/cdfm.py
@@ -42,7 +42,7 @@ class Diffusion:
         logging.info(f"Sampling {n} new images....")
         model.eval()
         with torch.no_grad():
-            x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
+            x = torch.randn((n, 1, self.img_size, self.img_size)).to(self.device)
             for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
                 t = (torch.ones(n) * i).long().to(self.device)
                 predicted_noise = model(x, t, labels)
@@ -100,7 +100,7 @@ def train(args):
             logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
 
         if epoch % 10 == 0:
-            labels = torch.arange(1).long().to(device)
+            labels = torch.rand(14,1).to(device).type(torch.float)
             sampled_images = diffusion.sample(model, n=len(labels), labels=labels)
             ema_sampled_images = diffusion.sample(ema_model, n=len(labels), labels=labels)
             plot_images(sampled_images)
