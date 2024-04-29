104582642a38fd6046302293f781a9d312c8907e
Alejandro-neuro <m.a.castanedagraci@tudelft.nl>
2024-04-30 01:01:24 +0200

    change norm by cdist

--- src/models/PhysModels.py
+++ src/models/PhysModels.py
@@ -94,3 +94,44 @@ class Sprin_ode(nn.Module):
       z_hat = torch.cat([pos1_t2 ,pos2_t2],dim=1).unsqueeze(1)
 
       return  z_hat
+    
+class gravity_ode(nn.Module):
+    def __init__(self, initw = False):
+        super().__init__()
+        self.g = torch.tensor([1.0], requires_grad=True).float()
+        self.g = nn.Parameter(self.g )
+
+        
+
+    def forward(self, z,dt):    
+      #print("z",z.shape)
+      
+      #z = z.reshape(-1, 2, 2)
+
+      device = "cuda" if torch.cuda.is_available() else "cpu"
+      dt = torch.tensor([dt], requires_grad=False).float().to(device)
+
+      pos_t0 = z[:,0,:]
+      pos_t1 = z[:,1,:]
+
+      vel = (pos_t1 - pos_t0)/dt
+
+      
+      
+      norm = torch.norm(pos1_t0 - pos2_t0, dim=1, keepdim=True) 
+      direction = (( pos1_t1 - pos2_t1 )/ (norm+1e-4)  )
+      
+
+      #if torch.isnan(direction).any():
+       
+        
+
+      #force  = self.k*(norm - 2*self.l)*direction
+      force = -(self.k*( pos1_t1 - pos2_t1 ) + self.l*direction)
+
+      pos1_t2 = pos1_t1 + vel1_t1*dt + force*dt*dt
+      pos2_t2 = pos2_t1 + vel2_t1*dt - force*dt*dt
+      
+      z_hat = torch.cat([pos1_t2 ,pos2_t2],dim=1).unsqueeze(1)
+
+      return  z_hat
--- src/models/encoders.py
+++ src/models/encoders.py
@@ -9,9 +9,9 @@ class EncoderMLP(nn.Module):
     def __init__(self, in_size = 50,in_chan = 1,  latent_dim=1, initw = False):
         super().__init__()
 
-        self.l1 = nn.Linear(in_size*in_size*in_chan,500 )
-        self.l2 = nn.Linear(500,100 )
-        self.l3 = nn.Linear(100,latent_dim)
+        self.l1 = nn.Linear(in_size*in_size*in_chan,500*in_chan )
+        self.l2 = nn.Linear(500*in_chan,100*in_chan )
+        self.l3 = nn.Linear(100*in_chan,latent_dim)
 
         self.relu= nn.ReLU()
         self.Softmax= nn.Softmax(dim=1)
